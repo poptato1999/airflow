36a0ffa8c454158f902f00d774dd632c90573a1d
poptato <poptato1999@gmail.com>
2023-11-18 11:24:42 +0900

    bash operator dag 업로드

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -7,7 +7,7 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.empty import EmptyOperator
 
 with DAG(
-    dag_id="example_bash_operator",
+    dag_id="dag_bash_operator",
     schedule="0 0 * * *",
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
     catchup=False,
