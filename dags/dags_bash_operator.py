1353407abfffe3ab1ccc1467fc2d06987a76ffe5
poptato <poptato1999@gmail.com>
2023-11-25 11:37:55 +0900

    python bash xcom

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -3,28 +3,32 @@ import datetime
 import pendulum
 
 from airflow import DAG
+from airflow.decorators import task
 from airflow.operators.bash import BashOperator
 from airflow.operators.empty import EmptyOperator
 
 with DAG(
-    dag_id="dag_bash_operator",
+    dag_id="1_dag_bash_python_with_xcom",
     schedule="0 0 * * *",
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
     catchup=False,
     dagrun_timeout=datetime.timedelta(minutes=60),
 ) as dag:
-    bash_push = BashOperator(
-        task_id='bash_push',
-        bash_command = "echo START && "
-                        "echo XCOM_PUSHED"
-                        "{{ ti.xcom_push(key='bash_pushed', value='first_bash_message')}} &&"
-                        "echo COMPLETE"
-    )
+    
+    @task(task_id='python_push')
+    def python_push_xcom():
+        result_dict = {'status': 'Good', 'data': [1,2,3], 'options_cnt':100}
+        return result_dict
+    
     bash_pull = BashOperator(
         task_id='bash_pull',
-        env={'PUSHED_VALUE': "{{ti.xcom_pull(key='bash_pushed')}}",
-             'RETURN_VALUE': "{{ti.xcom_pull(task_ids='bash_push')}}"}
-        bash_command = "echo $PUSHED_VALUE && echo $RETURN_VALUE ",
-        do_xcom_push=False
+        env={
+            'STATUS': '{{ti.xcom_pull(task_ids="python_push")["status"]}}',
+            'DATA': '{{ti.xcom_pull(task_ids="python_push")["data"]}}',
+            'OPTIONS_CNT': '{{ti.xcom_pull(task_ids="python_push")["options_cnt"]}}',
+        },
+        
+        bash_command='echo $STATUS && echo $DATA && echo $OPTIONS_CNT'
     )
-    bash_push >> bash_pull
+    
+    python_push_xcom() >> bash_pull
