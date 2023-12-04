035b085ba8b8eed5798bf35c0db80b9c70f21be8
poptato <poptato1999@gmail.com>
2023-12-04 18:48:46 +0900

    python

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -6,29 +6,51 @@ from airflow import DAG
 from airflow.decorators import task
 from airflow.operators.bash import BashOperator
 from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import BranchPythonOperator
+from airflow.operators.python import PythonOperator
 
 with DAG(
     dag_id="1_dag_bash_python_with_xcom",
     schedule="0 0 * * *",
-    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    start_date=pendulum.datetime(2023, 12, 4, tz="UTC"),
     catchup=False,
     dagrun_timeout=datetime.timedelta(minutes=60),
 ) as dag:
     
-    @task(task_id='python_push')
-    def python_push_xcom():
-        result_dict = {'status': 'Good', 'data': [1,2,3], 'options_cnt':100}
-        return result_dict
-    
-    bash_pull = BashOperator(
-        task_id='bash_pull',
-        env={
-            'STATUS': '{{ti.xcom_pull(task_ids="python_push")["status"]}}',
-            'DATA': '{{ti.xcom_pull(task_ids="python_push")["data"]}}',
-            'OPTIONS_CNT': '{{ti.xcom_pull(task_ids="python_push")["options_cnt"]}}',
-        },
+    def select_random():
+        import random 
         
-        bash_command='echo $STATUS && echo $DATA && echo $OPTIONS_CNT'
+        item_lst = ['A', 'B', 'C']
+        selected_item = random.choice(item_lst)
+        if selected_item == 'A':
+            return 'task_a'
+        elif selected_item in['B', 'C']:
+            return ['task_b', 'task_c']
+        
+        
+    python_branch_task = BranchPythonOperator(
+        task_id='python_branch_task',
+        python_callable=select_random
     )
     
-    python_push_xcom() >> bash_pull
+    def common_func(**kwargs):
+        print(kwargs['selected'])
+        
+    task_a = PythonOperator(
+        task_id='task_a',
+        python_callable=common_func,
+        op_kwargs={'selected': 'A'}
+    )
+        
+    task_b = PythonOperator(
+        task_id='task_b',
+        python_callable=common_func,
+        op_kwargs={'selected': 'B'}
+    )
+        
+    task_c = PythonOperator(
+        task_id='task_c',
+        python_callable=common_func,
+        op_kwargs={'selected': 'C'}
+    )
+    python_branch_task >> [task_a, task_b, task_c]
