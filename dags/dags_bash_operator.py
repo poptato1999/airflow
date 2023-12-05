35ee71c17e506c18c79970ca95157ae880fcfda5
poptato <poptato1999@gmail.com>
2023-12-05 11:40:38 +0900

    trigger

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -8,6 +8,8 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import BranchPythonOperator
 from airflow.operators.python import PythonOperator
+from airflow.operators.branch import BaseBranchOperator
+from airflow.exceptions import AirflowException
 
 with DAG(
     dag_id="1_dag",
@@ -17,36 +19,21 @@ with DAG(
     dagrun_timeout=datetime.timedelta(minutes=60),
 ) as dag:
     
-    @task.branch(task_id='python_branch_task')
-    def select_random():
-        import random 
-        
-        item_lst = ['A', 'B', 'C']
-        selected_item = random.choice(item_lst)
-        if selected_item == 'A':
-            return 'task_a'
-        elif selected_item in['B', 'C']:
-            return ['task_b', 'task_c']
-
+    bash_upstream_1 = BashOperator(
+        task_id='bash_upstream_1',
+        bash_command='echo upstream1'
+    )
     
-    def common_func(**kwargs):
-        print(kwargs['selected'])
+    @task(task_id='python_upstream_1')
+    def python_upstream_1():
+        raise AirflowException('downstream_1 Exception!')
+    
+    @task(task_id='python_upstream_2')
+    def python_upstream_2():
+        print('정상처리')
+    
+    @task(task_id='python_downstream_1', trigger_rule='all_done')
+    def python_downstream_1():
+        print('정상처리')
         
-    task_a = PythonOperator(
-        task_id='task_a',
-        python_callable=common_func,
-        op_kwargs={'selected': 'A'}
-    )
-        
-    task_b = PythonOperator(
-        task_id='task_b',
-        python_callable=common_func,
-        op_kwargs={'selected': 'B'}
-    )
-        
-    task_c = PythonOperator(
-        task_id='task_c',
-        python_callable=common_func,
-        op_kwargs={'selected': 'C'}
-    )
-    select_random() >> [task_a, task_b, task_c]
+    [bash_upstream_1, python_upstream_1(), python_upstream_2()] >> python_downstream_1
